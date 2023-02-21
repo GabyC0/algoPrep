bc39e904f0347d3af2c735e35013fef0bf27046c
Gaby Campos <gabycamposcontact@gmail.com>
2023-02-20 16:33:03 -0800

    seek and destroy intermediate algo

--- index.js
+++ index.js
@@ -210,3 +210,18 @@ function pairElement(str) {
 }
 
 pairElement("ATCGA");
+
+
+//Seek and Destroy
+
+//You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.
+//Note: You have to use the arguments object.
+
+function destroyer(arr) {
+  //arr includes an array and more arguments, slice args after the initial arr (1) and add them to a deleted nums var
+  let delNums = Array.from(arguments).splice(1);
+  //console.log('delNums', delNums);
+  //return filtered elements in arr if they're not included in delNums
+  return arr.filter((elem) => !delNums.includes(elem));
+}
+destroyer([1, 2, 3, 1, 2, 3], 4, 3);
